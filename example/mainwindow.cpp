f957b5a870c3fa7db1a4d73d66d80581bf03c3f5
Jacob Dawid <jacob.dawid@9elements.com>
2015-01-27 13:32:48 +0100

    Further improved example.

--- example/mainwindow.cpp
+++ example/mainwindow.cpp
@@ -1,8 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QUrl>
+
 MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
+    QMainWindow(parent, Qt::WindowStaysOnTopHint | Qt::CustomizeWindowHint | Qt::FramelessWindowHint),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
@@ -10,8 +12,10 @@ MainWindow::MainWindow(QWidget *parent) :
     _showCounter = 0;
     _systemTrayIcon.setDarkThemePixmap(QPixmap(":/images/images/icon_dark.png"));
     _systemTrayIcon.setLightThemePixmap(QPixmap(":/images/images/icon_light.png"));
-
+    _systemTrayIcon.setText("0");
     connect(&_systemTrayIcon, SIGNAL(trayIconToggled(QRect)), this, SLOT(trayIconToggled(QRect)));
+
+    ui->webView->load(QUrl("http://www.google.de"));
 }
 
 MainWindow::~MainWindow()
@@ -23,10 +27,10 @@ void MainWindow::trayIconToggled(QRect geometry) {
     _systemTrayIcon.setText(QString("%1").arg(_showCounter));
 
     if(isVisible()) {
+        _showCounter++;
         hide();
     } else {
-        move(geometry.x() - width() / 2, geometry.y());
-        _showCounter++;
+        move(geometry.x() - width() / 2 + geometry.width() / 2, geometry.y());
         show();
     }
 }
